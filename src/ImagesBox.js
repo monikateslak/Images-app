75bc16fb062683bf5f253abd31db7c910d36ae31
Monika Mona <mona.mt.89@gmail.com>
2022-08-26 11:43:19 +0200

    add images component

--- src/ImagesBox.js
+++ src/ImagesBox.js
@@ -1,12 +1,41 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import logo from './logo.svg'
-import {ReactSVG} from 'react-svg'
+//import {ReactSVG} from 'react-svg'
 
 const ImagesBox = () => {
+  const [images, setImages] = useState (
+    [
+      {
+        name: "szałwia",
+        price: "5,00 zł",
+        id: 1
+      },
+      {
+        name: "melisa",
+        price: "5,00 zł",
+        id: 2
+      },
+      {
+        name: "oregano",
+        price: "5,00 zł",
+        id: 3
+    
+      },
+
+    ]
+  )
+
   return (
     <div id="images-box">
       <img src={logo} width="100" height="100" alt="logo"></img>
+      <main>
+        {images.map(images => (
+          <ul>
+            <li>{images.name}</li>
+          </ul>
+        ))}
+      </main>
     </div>
   )
 }
